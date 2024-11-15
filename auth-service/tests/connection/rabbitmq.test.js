2aea79ef0dbbeb69375bcec26f26d9da927a8f22
heronoa <heron.amaral@gmail.com>
2024-11-15 15:21:42 -0300

    feat: creating auth-service tests

---
+++ auth-service/tests/connection/rabbitmq.test.js
@@ -0,0 +1,40 @@
+const amqp = require("amqplib");
+
+describe("RabbitMQ Integration Test", () => {
+  let connection;
+  let channel;
+  const queue = "test_queue";
+  const message = { text: "Hello, RabbitMQ!" };
+
+  beforeAll(async () => {
+    connection = await amqp.connect(process.env.RABBITMQ_URL);
+    channel = await connection.createChannel();
+
+    await channel.assertQueue(queue);
+  });
+
+  afterAll(async () => {
+    await channel.close();
+    await connection.close();
+  });
+
+  it("It should send and receive RabbitMQ message", async () => {
+    await channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+
+    const receivedMessage = await new Promise((resolve) => {
+      channel.consume(
+        queue,
+        (msg) => {
+          if (msg !== null) {
+            resolve(JSON.parse(msg.content.toString()));
+            channel.ack(msg); // Remove a mensagem da fila
+          }
+        },
+        { noAck: false }
+      );
+    });
+
+    // Verifica se a mensagem recebida é igual à mensagem enviada
+    expect(receivedMessage).toEqual(message);
+  });
+});
---
+++ auth-service/tests/integration/userMocks.js
@@ -0,0 +1,24 @@
+const usersMocks = [
+  {
+    name: "User 1",
+    email: "test@example.com",
+    password: "encrypted-hash",
+    username: "tester1",
+  },
+  {
+    name: "User 2",
+    email: "test2@example.com",
+    password: "encrypted-hash2",
+    username: "tester2",
+    role: "user",
+  },
+  {
+    name: "User 3",
+    email: "test3@example.com",
+    password: "encrypted-hash3",
+    username: "tester3",
+    role: "user",
+  },
+];
+
+module.exports = usersMocks;
